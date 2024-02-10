0dd11688b696bdd10e6cee276553ad4d42c275f0
Karan Garg <121216986+Karangarg01@users.noreply.github.com>
2024-02-10 20:11:47 +0530

    Update StarPattern.java

--- StarPattern.java
+++ StarPattern.java
@@ -23,5 +23,96 @@ public class Main
 		    System.out.println();
 		}
 		
-	}
+        int rows;
+        Scanner sc = new Scanner(System.in);
+        rows = sc.nextInt();
+        
+        
+        // ButterFly Pattern
+        // for(int i=1;i<=rows;i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+            
+        //     int space = 2*(rows-i);
+        //     for(int k=1; k<=space;k++){
+        //         System.out.print(" ");
+        //     }
+            
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }System.out.println();    
+        // }
+        
+        // for(int i=rows; i>=1; i--){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+            
+        //     int space = 2*(rows-i);
+        //     for(int k=1; k<=space;k++){
+        //         System.out.print(" ");
+        //     }
+            
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }System.out.println();    
+        // }
+        
+
+
+        // Rhomboid Pattern
+        // for(int i=1;i<=rows;i++){
+        //     int spaces = rows-i;
+        //     for(int j=1; j<=spaces;j++){
+                
+        //         System.out.print(" ");
+        //     }
+            
+        //     for(int j=1; j<=rows;j++){
+                
+        //         System.out.print("*");
+        //     }System.out.println();
+            
+            
+            
+ 
+ 
+        //Number Pyramid
+        // for(int i=1;i<=rows;i++){
+            
+        //     for(int j=1; j<=rows-i;j++){
+                
+        //         System.out.print(" ");
+        //     }
+            
+        //     for(int j=1; j<=i;j++){
+                
+        //         System.out.print(i + " ");
+        //     }System.out.println();           
+            
+            
+        // }
+        
+  
+        for(int i=1;i<=rows;i++){
+            
+            for(int j=1; j<=rows-i;j++){
+                
+                System.out.print(" ");
+            }
+            
+            int num = i; 
+            
+            for(int j=1; j<=i;j++){
+                
+                System.out.print(num + " ");
+                num+=j;
+            }System.out.println();           
+            
+            
+        }   
+          
+    }
 }
+
